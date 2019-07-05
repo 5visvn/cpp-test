bc6d87bb2c26b8d3641ecbdcaff8eae3c8f71c37
wisan <wisan@vip.qq.com>
2019-07-06 01:04:36 +0800

    Add comments

--- learn_test/PotIteratorOperationSelector.cc
+++ learn_test/PotIteratorOperationSelector.cc
@@ -1,3 +1,8 @@
+// requirements:
+// ip pot has two type of protocol, smp pot only one protocol
+// each types has a switch to control operation "remove" or "reauth"
+// for ip pot, need to check every data for different operations
+// for smp pot, need to check only once.
 #include <set>
 #include <vector>
 #include <functional>
@@ -16,35 +21,37 @@ public:
       typedef std::function<void()> Method;
 
       std::vector<Method> cleanMethods;
-      // ping RAR, index is 0
-      auto pingRAR = [&](){
+      // reauth
+      auto ping = [&](){
          reauth(protocol, sessionId);
       };
-      // remove, index is 1
+      // remove
       auto remove = [&](){
          remove(protocol, sessions);
       };
-      // remove and pingRAR selector, index is 2
+      // remove and pingRAR selector
       auto methodSelector = [&](){
-         if ((protocol == "Gx" && check4G) || (protocol == "Smf" && check5G))
-            pingRAR();
-         else if ((protocol == "Gx" && check4G) || (protocol == "Smf" && check5G))
+                               if ((protocol == "4G" && check4G) || (protocol == "5G" && check5G)
+            ping();
+                                   if ((protocol == "4G" && !check4G) || (protocol == "5G" && !check5G)
             remove();
       };
 
       Method method;
       std::string potType{};
+      // different pot, different solution
       if (potType == "IpPot")
          method = std::move(methodSelector);
       else if (potType == "SmpPot")
       {
          if (check4G)
-            method = std::move(pingRAR);
+            method = std::move(ping);
          else
             method = std::move(remove);
       }
 
-      while (sessions.size() > 0)// iterate pot
+      // iterate pot
+      while (sessions.size() > 0)
       {
          protocol = ""; // get protocol
          // do ping or remove when smp
