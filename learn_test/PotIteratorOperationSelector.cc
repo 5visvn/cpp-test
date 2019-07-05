01afb31f28dcad9fb520cbad612e102b042abbb9
wisan <wisan@vip.qq.com>
2019-07-06 00:16:00 +0800

    Pot Method selector

---
+++ learn_test/PotIteratorOperationSelector.cc
@@ -0,0 +1,65 @@
+#include <set>
+#include <vector>
+#include <functional>
+
+class Cleaner{
+   void removeSession(std::string p, std::set<int> s){}
+   void reauthSession(std::string p, std::string s){}
+public:
+   void doClean()
+   {
+      std::set<int> sessions;
+      std::string protocol{};
+      std::string sessionId{};
+      bool check4G{true};
+      bool check5G{true};
+      typedef std::function<void()> Method;
+
+      std::vector<Method> cleanMethods;
+      // ping RAR, index is 0
+      cleanMethods.push_back(
+         std::move([&](){
+            reauthSession(protocol, sessionId);
+         }));
+      // remove, index is 1
+      cleanMethods.push_back(
+         std::move([&](){
+            removeSession(protocol, sessions);
+         }));
+      // remove and pingRAR selector, index is 2
+      cleanMethods.push_back(
+         std::move([&](){
+            if ((protocol == "Gx" && check4G) || (protocol == "Smf" && check5G))
+               cleanMethods[0]();
+            else if ((protocol == "Gx" && check4G) || (protocol == "Smf" && check5G))
+               cleanMethods[1]();
+         }));
+
+      // std::function<void* ()> cleanSession;
+      int methodIndex{0};
+      std::string potType{};
+      if (potType == "IpPot")
+         methodIndex = 2;
+      else if (potType == "SmpPot")
+      {
+         if (check4G)
+            methodIndex = 0;
+         else
+            methodIndex = 1;
+      }
+
+      while (sessions.size() > 0)// iterate pot
+      {
+         protocol = ""; // get protocol
+         // do ping or remove when smp
+         // do selector when ip
+         cleanMethods[methodIndex]();
+      }
+   }
+};
+
+int main()
+{
+   Cleaner c;
+   c.doClean();
+}
