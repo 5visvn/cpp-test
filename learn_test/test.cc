c2c4e26f7ed8b78d1a6a93b20b85e6b4d36c9335
wisan <wisan@vip.qq.com>
2019-11-26 15:39:17 +0800

    update test

--- learn_test/test.cc
+++ learn_test/test.cc
@@ -3,46 +3,9 @@
 
 using namespace std;
 
-struct conf{
-   int num;
-   string attr;
-   conf():num(1), attr("hi"){}
-
-   conf(const conf & c):
-      num(c.num),
-      attr(c.attr)
-   {}
-
-};
-
-
-
-class Singleton{
-   Singleton():a(2){}
-   virtual ~Singleton(){}
-   static Singleton * instance;
-   int a;
-public:
-   static Singleton * getInstance(){
-      if (!instance){
-         instance = new Singleton();
-      }
-      return instance;
-   }
-
-   void print(){
-      conf c;
-      auto l = [co=c, this](){
-                  cout << co.attr << endl << a << endl;
-               };
-      auto l1 = std::move(l);
-      l1();
-   }
-};
-
-Singleton * Singleton::instance = nullptr;
-
-
-int main(){
-   Singleton::getInstance()->print();
+int main()
+{
+   string input = "imeisv-19290012313";
+   auto index = input.find_first_of('-');
+   cout << input.substr(index + 1, input.size());
 }
