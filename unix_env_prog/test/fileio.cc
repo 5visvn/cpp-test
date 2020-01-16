cf6081ce23f99bbb7851ab74c1eb376560506360
wisan <wisan@vip.qq.com>
2020-01-16 23:30:44 +0800

    file ios

--- unix_env_prog/test/fileio.cc
+++ unix_env_prog/test/fileio.cc
@@ -1,4 +1,4 @@
-#include <apue.h>
+#include "../apue.3e/include/apue.h"
 
 
 void pr_stdio(const char*, FILE*);
@@ -14,4 +14,110 @@ int main(void)
 
    if (getchar() == EOF)
       err_sys("getchar error");
+   fputs("one line to standard error\n", stderr);
+
+   pr_stdio("stdin", stdin);
+   pr_stdio("stdout", stdout);
+   pr_stdio("stderr", stderr);
+
+   if ((fp = fopen("/etc/passwd", "r")) == NULL)
+      err_sys("fopen error");
+
+   if (getc(fp) == EOF)
+      err_sys("getc error");
+
+   pr_stdio("/etc/passwd", fp);
+   exit(0);
 }
+
+void
+pr_stdio(const char* name, FILE* fp)
+{
+   printf("stream = %s, \n", name);
+   if (is_unbuffered(fp))
+      printf("unbuffered");
+   else if (is_linebuffered(fp))
+      printf("linebuffered");
+   else
+      printf("fully buffered");
+   printf(", buffer size = %d\n", buffer_size(fp));
+}
+
+#if defined(_IO_UNBUFFERED)
+
+int
+is_unbuffered(FILE* fp)
+{
+   return (fp->_flags & _IO_UNBUFFERED);
+}
+
+int
+is_linebuffered(FILE* fp)
+{
+   return (fp->_flags & _IO_LINE_BUF);
+}
+
+int
+buffer_size(FILE* fp)
+{
+   return (fp->_IO_buf_end - fp->_IO_buf_base);
+}
+
+#elif defined(_SNBF)
+
+int
+is_unbuffered(FILE* fp)
+{
+   return (fp->_flags & __SNBF);
+}
+
+int
+is_linebuffered(FILE* fp)
+{
+   return (fp->_flags & __SLBF);
+}
+
+int
+buffer_size(FILE* fp)
+{
+   return (fp->_bf._size);
+}
+
+#elif defined(_IONBF)
+// respberry pi works
+
+#ifdef _LP64
+#define _flag __pad[4]
+#define _ptr __pad[1]
+#define _base __pad[2]
+#endif
+
+int
+is_unbuffered(FILE* fp)
+{
+   return (fp->_flags & _IONBF);
+}
+
+int
+is_linebuffered(FILE* fp)
+{
+   return (fp->_flags & _IOLBF);
+}
+
+int
+buffer_size(FILE* fp)
+{
+#ifdef _LP64
+   return (fp->_base - fp->_ptr);
+#else
+   // printf("Just a guess"); // pi works this way
+   return (BUFSIZ); // just a guess
+#endif
+}
+
+
+#else
+
+#error unknown stdio implementation!
+
+#endif
